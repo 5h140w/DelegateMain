9c856552336ff31e6474bb7724d21c5ba016807c
5h140w <5h140w23@gmail.com>
2022-06-21 22:47:21 +0100

    correct some error

--- src/components/unwrap_main/Unwrap_main.jsx
+++ src/components/unwrap_main/Unwrap_main.jsx
@@ -4,11 +4,11 @@ import "./unwrap_main.css"
 const Unwrap_main = () =>{
     return(
         <div className='unwrap_section'>
-          <h1> UnWrap</h1>
+          <h1> Unwrap</h1>
           <div className='input_section'>
             <input type="text" placeholder="Unwrap your SGB"/>
           </div>
-          <button className='wrap_btn'>Unrap</button>
+          <button className='wrap_btn'>Unwrap</button>
         </div>
     )
 }
