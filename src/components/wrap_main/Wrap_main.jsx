a2b075446f1a501acf0bf5ab41bdeb92745c0bfa
5h140w <5h140w23@gmail.com>
2022-06-22 13:11:20 +0100

    add wrap functions

--- src/components/wrap_main/Wrap_main.jsx
+++ src/components/wrap_main/Wrap_main.jsx
@@ -1,12 +1,59 @@
-import React from 'react'
+import { ethers } from 'ethers'
+import React, { useContext, useEffect, useState } from 'react'
+import AppState from '../../AppState'
+import { getSigner, wrap } from '../../logic/web3'
 import "./wrap_main.css"
 
 const Wrap_main = () =>{
+    const context = useContext(AppState)
+    const [appState, setAppState] = useState({loading: false})
+    const [balance, setbalance] = useState(0)
+
+    useEffect(
+      ()=>{
+        const getBalance= async () =>{
+          setAppState({loading:true})
+          const balance = parseFloat( ethers.utils.formatUnits(await getSigner().getBalance() , 18))
+          setbalance(balance)
+          setAppState({loading: false})
+        }
+        getBalance()
+      },[]
+    )
+
+    const WrapTx = async()=>{
+      const wrap_amount = document.getElementById("wrap_amount").value
+      if (!wrap_amount || parseFloat(wrap_amount) <= 0) return;
+      setAppState({loading: true})
+      const result = await wrap( wrap_amount)
+                .catch((e)=>{
+                  return e
+                })
+      context.reset()
+      setAppState({loading: false, result})          
+    }  
+
+    const setMax = ()=>{
+      if(balance<= 0.5) return;
+      document.getElementById("wrap_amount").value = document.getElementById("wrap_amount").max
+    }
+
     return(
         <div className='wrap_section'>
           <h1> Wrap</h1>
           <div className='input_section'>
-            <input type="text" placeholder="Wrap your SGB"/>
+            <input 
+              type="number" 
+              placeholder="Wrap your SGB"
+              id="wrap_amount"
+              min={0}
+              max={balance-0.5}
+            />
+            <button
+              onClick={setMax}
+            > 
+              Max
+            </button>
           </div>
           <button className='wrap_btn'>Wrap</button>
         </div>
